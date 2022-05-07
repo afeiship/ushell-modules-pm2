150cb31448120de375241904ca9684d4da365432
afeiship <1290657123@qq.com>
2022-05-07 16:44:09 +0800

    Release 1.0.3

--- index.sh
+++ index.sh
@@ -2,17 +2,24 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
-alias pm2-ls='pm2 list';
-alias pm2-st='pm2 start';
-alias pm2-sp='pm2 stop';
-alias pm2-rt='pm2 restart';
-alias pm2-del='pm2 delete';
+# l: list
+# s: start
+# p: stop
+# r: restart
+# d: delete
+# rt: delete
+alias pm2l='pm2 list';
+alias pm2s='pm2 start';
+alias pm2p='pm2 stop';
+alias pm2r='pm2 restart';
+alias pm2d='pm2 delete';
 alias pm2-logs='pm2 logs';
-
-alias pm2-stt='pm2 start all';
-alias pm2-spp='pm2 stop all';
-alias pm2-dell='pm2 delete all';
-alias pm2-rs="pm2 reset all";
 alias pm22='pm2 start ecosystem.config.js';
+
+alias pm2ss='pm2 start all';
+alias pm2pp='pm2 stop all';
+alias pm2rr='pm2 restart all';
+alias pm2dd='pm2 delete all';
+alias pm2rt="pm2 reset all";
 
 unset ROOT_PATH;
